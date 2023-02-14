9504ca34643a5ec1e16028294c57e7cb1aafc5b8
Abhik Banerjee <abhik.banerjee.1999@gmail.com>
2023-02-14 15:23:25 +0530

    Added Factory Contract

--- contracts/JAGDexPair.sol
+++ contracts/JAGDexPair.sol
@@ -143,8 +143,8 @@ contract JAGDexPair is IJAGDexPair, JAGDexERC20 {
         (uint112 _reserve0, uint112 _reserve1, ) = getReserves(); // gas savings
         uint balance0 = IERC20(token0).balanceOf(address(this));
         uint balance1 = IERC20(token1).balanceOf(address(this));
-        uint amount0 = balance0 -_reserve0;
-        uint amount1 = balance1-_reserve1;
+        uint amount0 = balance0 - _reserve0;
+        uint amount1 = balance1 - _reserve1;
 
         bool feeOn = _mintFee(_reserve0, _reserve1);
         uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
@@ -153,15 +153,15 @@ contract JAGDexPair is IJAGDexPair, JAGDexERC20 {
             _mint(address(0), MINIMUM_LIQUIDITY); // permanently lock the first MINIMUM_LIQUIDITY tokens
         } else {
             liquidity = Math.min(
-                (amount0*_totalSupply) / _reserve0,
-                (amount1*_totalSupply) / _reserve1
+                (amount0 * _totalSupply) / _reserve0,
+                (amount1 * _totalSupply) / _reserve1
             );
         }
         require(liquidity > 0, "JAGDex: INSUFFICIENT_LIQUIDITY_MINTED");
         _mint(to, liquidity);
 
         _update(balance0, balance1, _reserve0, _reserve1);
-        if (feeOn) kLast = uint(reserve0)*reserve1; // reserve0 and reserve1 are up-to-date
+        if (feeOn) kLast = uint(reserve0) * reserve1; // reserve0 and reserve1 are up-to-date
         emit Mint(msg.sender, amount0, amount1);
     }
 
@@ -178,8 +178,8 @@ contract JAGDexPair is IJAGDexPair, JAGDexERC20 {
 
         bool feeOn = _mintFee(_reserve0, _reserve1);
         uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
-        amount0 = (liquidity*balance0) / _totalSupply; // using balances ensures pro-rata distribution
-        amount1 = (liquidity*balance1) / _totalSupply; // using balances ensures pro-rata distribution
+        amount0 = (liquidity * balance0) / _totalSupply; // using balances ensures pro-rata distribution
+        amount1 = (liquidity * balance1) / _totalSupply; // using balances ensures pro-rata distribution
         require(
             amount0 > 0 && amount1 > 0,
             "JAGDex: INSUFFICIENT_LIQUIDITY_BURNED"
@@ -191,7 +191,7 @@ contract JAGDexPair is IJAGDexPair, JAGDexERC20 {
         balance1 = IERC20(_token1).balanceOf(address(this));
 
         _update(balance0, balance1, _reserve0, _reserve1);
-        if (feeOn) kLast = uint(reserve0) *reserve1; // reserve0 and reserve1 are up-to-date
+        if (feeOn) kLast = uint(reserve0) * reserve1; // reserve0 and reserve1 are up-to-date
         emit Burn(msg.sender, amount0, amount1, to);
     }
 
@@ -243,11 +243,11 @@ contract JAGDexPair is IJAGDexPair, JAGDexERC20 {
         );
         {
             // scope for reserve{0,1}Adjusted, avoids stack too deep errors
-            uint balance0Adjusted = (balance0 * 1000) - (amount0In*3);
-            uint balance1Adjusted = (balance1 * 1000) - (amount1In*3);
+            uint balance0Adjusted = (balance0 * 1000) - (amount0In * 3);
+            uint balance1Adjusted = (balance1 * 1000) - (amount1In * 3);
             require(
                 (balance0Adjusted * balance1Adjusted) >=
-                    ((uint(_reserve0) *_reserve1) * 1000 ** 2),
+                    ((uint(_reserve0) * _reserve1) * 1000 ** 2),
                 "JAGDex: K"
             );
         }
